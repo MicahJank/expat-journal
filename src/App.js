35821e2e4ae7a2774db4a8960d3f73bd606c8e89
John Turner <johneturner@gmail.com>
2019-10-16 22:50:46 -0400

    correctly set up new less files for login page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 
+import LoginPage from './components/LoginPage';
+
 function App() {
   return (
     <div className="App">
-      {/* <Route exact path="/login" component={LoginPage} /> */}
+      <Route exact path="/login" component={LoginPage} />
     </div>
   );
 }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import '../css/index.css';
 
-export default function LoginPage() {}
+export default function LoginPage() {
+  return (
+    <div>
+      <h3>Temp header</h3>
+    </div>
+  );
+}
