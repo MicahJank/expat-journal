6f836e20ac83fa42439eb77691508db7eafb0d00
MicahJank <MicahJank@users.noreply.github.com>
2019-10-17 14:10:33 -0500

    began some styling for the navigation bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -11,14 +11,16 @@ const NavigationBar = () => {
 
     return (
         <div className='navigation-bar'>
-            <div>
+            <div className='nav-top'>
                 <img src={logo} alt='expat journal logo' />
+                <NavLink to={'/profile'}>UserName</NavLink>
             </div>
-            <div>
+            <div className='nav-bottom'>
                 <NavLink to={'/newsfeed'}>NewsFeed</NavLink>
                 <NavLink to={'/saved-content'}>Saved content</NavLink>
                 <NavLink to={'/upload'}>Upload</NavLink>
                 <NavLink to={'/notifications'}>Notifications</NavLink>
+                <input className='nav-search' placeholder='Search country or city' />
             </div>
         </div>
     )
