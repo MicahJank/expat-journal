f4aefcfabd18b3444e0f4d704cf27a5d8b084e36
Jashele T <jashele@Jasheles-Air.domain>
2019-10-17 19:09:52 -0700

    added actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,8 @@ export const FETCH_STORY_ERROR = "FETCH_STORY_ERROR";
 export const ADD_STORY = "ADD_STORY";
 export const ADD_STORY_SUCCESS = "ADD_STORY_SUCCESS";
 export const ADD_STORY_FAIL = "ADD_STORY_FAIL";
+export const EDIT_STORY = "EDIT_STORY";
+export const DELETE_STORY= "DELETE_STORY";
 
 // FETCH STORIES FROM API
 
@@ -40,4 +42,24 @@ export const addStory = add => dispatch => {
 };
 
 // EDIT EXISTING STORY
+
+export const editStory = edit => dispatch => {
+  axios
+    .put("https://jsonplaceholder.typicode.com/todos/id", edit)
+    .then( res => {
+        dispatch({ type: EDIT_STORY, payload: res});
+    })
+    .catch(err => (console.log(err))
+    )};
+
+
 // DELETE EXISTING STORY
+
+export const deleteStory = del => dispatch => {
+  axios 
+    .delete("https://jsonplaceholder.typicode.com/todos/id", del) 
+    .then(res => {
+      dispatch({type: DELETE_STORY, payload: res});
+    })
+    .catch(err => (console.log(err))
+    )};
