da17e8254cd58f20f8100dcdf6391cd3c9f6d839
John Turner <johneturner@gmail.com>
2019-10-19 11:18:25 -0400

    add imgs for auth pages

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import '../css/index.css';
 
-export default function SignUpPage() {
-  console.log('Sign up page!');
+// import imgs
+import logo from '../imgs/LogoEJ.png';
+import group from '../imgs/group-svgrepo-com.png';
+import uploader from '../imgs/icon-uploader-2.png';
+import save from '../imgs/save-button-2.png';
+import background from '../imgs/typewriter-801921_1920.jpg';
 
+// <img src={logo} alt="expat journal logo" />
+
+// console to help me remember which page i'm working on!
+console.log('Sign up page!');
+
+export default function SignUpPage() {
   return (
     <div>
       <h2>Sign up to start your journal</h2>
