7aea05247a24197c07190bee28a0cf21fb93eb95
Jashele T <jashele@Jasheles-Air.domain>
2019-10-23 18:21:43 -0700

    all expats now go to unique url

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function Story() {
+function Story(props) {
   const [trip, setTrip] = useState([]);
 
   useEffect(() => {
     axios
-      .get(`https://pt11expat.herokuapp.com/api/stories/byId/2`)
+      .get(`https://pt11expat.herokuapp.com/api/stories/byId/${props.match.params.id}`)
       .then(res => {
         setTrip(res.data);
         console.log(res.data);
