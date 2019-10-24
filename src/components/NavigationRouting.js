db636862e20d7e1ed78d29f27fb0d1b6d25742fe
Jashele T <jashele@Jasheles-Air.domain>
2019-10-23 17:56:25 -0700

    troubleshooting conflict

--- src/components/NavigationRouting.js
+++ src/components/NavigationRouting.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-import React from "react";
-import { Route } from "react-router-dom";
-import UploadForm from "../components/UploadForm.js";
-import NewsFeed from "../components/Newsfeed.js";
-import FooterBar from "../components/Footer";
-import EditForm from "../components/EditForm.js";
-import Story from "../components/Stories";
-=======
 import React from 'react';
 import { Route } from 'react-router-dom';
 import UploadForm from '../components/UploadForm.js';
 import NewsFeed from '../components/Newsfeed.js';
 import EditForm from '../components/EditForm.js';
->>>>>>> cf3a11b786911f50f6aedaf514aa468d2acf7af1
+import Story from '../components/Stories';
 
 const NavigationRouting = () => {
 
