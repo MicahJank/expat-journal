9e633aae2592c33b6d37abbcaa045f3bb70899a7
MicahJank <MicahJank@users.noreply.github.com>
2019-10-24 11:32:43 -0500

    made it so user must be loggged in to access teh app, otherwise it redirects them to sign in

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
 import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute.js';
 
 // NavigationBar should be visible on all pages of the app so instead
 // of attaching it to a route via the component property i have imported it here
@@ -16,7 +17,7 @@ function App() {
       <Switch>
         <Route exact path="/sign-in" component={LoginPage} />
         <Route exact path="/sign-up" component={SignUpPage} />
-        <Route path="/" component={NavigationBar} />
+        <PrivateRoute path="/" component={NavigationBar} />
       </Switch>
       <Route path="/" component={Footer} />
     </div>
